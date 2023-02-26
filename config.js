0570eae0f2b1b2036e2b2b62babb8ec3f7123d3e
bluemoon-eggplant <m.nakahara@nehan.io>
2023-02-26 21:02:15 +0900

    全コンテンツページ配置まで

--- config.js
+++ config.js
@@ -56,6 +56,7 @@ const config = {
       '/schadule',
       '/schedule_notification',
       '/management_content',
+      '/against_blackbox',
       '/other_function',
     ],
     // collapsedNav: [''],
